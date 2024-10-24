9964d8f9383e614b2827a82f7ba938248ea432ec
RenanYMartins <yohanssen.martins@gmail.com>
2024-10-24 11:13:07 -0300

    :sparkles: model refeicoes

--- projeto_meals/meals/lib/screens/categories_meals_screen.dart
+++ projeto_meals/meals/lib/screens/categories_meals_screen.dart
@@ -1,29 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
+import '../data/dummy_data.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
-  const CategoriesMealsScreen({super.key });
-
+  const CategoriesMealsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final category = ModalRoute.of(context)!.settings.arguments as Category; // forma de receber o contexto da tela anterior sem precisar passar parametros
+    final category = ModalRoute.of(context)!.settings.arguments
+        as Category; // forma de receber o contexto da tela anterior sem precisar passar parametros
+    final categoryMeals = dummyMeals.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
+
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          category.title,
-          style: const TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-            fontWeight: FontWeight.bold, 
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            category.title,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
           ),
+          backgroundColor: Theme.of(context).colorScheme.primary,
         ),
-        backgroundColor: Theme.of(context).colorScheme.primary,
-      ),
-      body: Center(
-        child: Text("Receitas por categoria ${category.id}"),
-      )
-    );
+        body: ListView.builder(
+          itemCount: categoryMeals.length,
+          itemBuilder: (context, index){
+            return Text(categoryMeals[index].title);
+          },
+        ));
   }
 }
